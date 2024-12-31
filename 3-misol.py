f9bcbbeb828dc5cc416d364d50cb41335534f8c7
Hay1tov <eldorh35@gmail.com>
2024-12-31 13:16:05 +0500

    Bajarildi

---
+++ 3-misol.py
@@ -0,0 +1,4 @@
+1text = 5     # identifier raqam yoki har xil ishoralar bilan boshlanmaydi
+my-variable = 10  # identifierda chiziqcha " - " ishlatilmaydi
+for = "Hello"   # pythonda "if , else , while , for" bunday so'zlardan identifier sifatida foydalanib bo'lmaydi
+my variable = 20  # identifier yaratishda bo'sh joy bo'lmasligi kerak
