2ee7e47b109959afaa33e3e8910ddf533b76e591
Hay1tov <eldorh35@gmail.com>
2025-01-06 16:54:02 +0500

    Bajarildi

---
+++ 33-misol.py
@@ -0,0 +1,16 @@
+s = 12, 55.6, 56, 12, 15, 6, 8, 99, 55, 10
+max_num = 1
+
+for i in s:
+    n = s.count(i)
+    
+    if n > max_num:
+        max_num = n
+        maximum = i
+
+
+print(f"{maximum} -> {max_num} mmarta takrorlangan")
+
+      
+    
+        
